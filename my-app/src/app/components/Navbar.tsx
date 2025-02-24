36aea9a9d52efd9bcca725ff81cbbfd256bb6e54
tmt83019 <thu7560@gmail.com>
2025-02-23 22:40:09 -0500

    Updated navbar

--- my-app/src/app/components/Navbar.tsx
+++ my-app/src/app/components/Navbar.tsx
@@ -1,16 +1,32 @@
-import Link from 'next/link';
 import styles from './Navbar.module.css';
+import magnifyingGlass from '../images/magnifyingGlass.png';
+import Link from 'next/link';
+import Image from 'next/image';
+
 
 export default function Navbar() {
     return (
         <nav className={styles.navbar}>
-            <Link href='/'>SlayTix</Link>
-            <div>
-                <form>
-                    <input type="text" placeholder="Search..." name="search"/>
-                    <button type="submit">SearchIconHere</button>
-                </form>
+
+            {/* Left section of navbar */}
+            <div className={styles.leftSection}>
+                <Link className={styles.slaytix} href='/'>SlayTix</Link>
+                <div className={styles.search}>
+                    <form>
+                        <input type="text" placeholder="Search..."/>
+                        <button type="submit"><Image className={styles.searchButton} src={magnifyingGlass} alt="Magnifying glass"/></button>
+                    </form>
+                </div>
             </div>
-        </nav>  
+
+            {/* Links on right side of navbar */}
+            <ul className={styles.links}>
+                <li><Link href='/filterMovie'>Movies</Link></li>
+                <li><Link href='/'></Link>Promotions</li>
+                <li><Link href='/loginUser'>Log In</Link></li>
+                <li><Link href='/registerPersonal'>Sign Up</Link></li>
+            </ul>
+
+        </nav> 
     );
 }
