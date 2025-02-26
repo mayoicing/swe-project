05a8a4f6d2078193ad276ffa9ed665664e7dde24
tmt83019 <thu7560@gmail.com>
2025-02-26 18:42:15 -0500

    Updated styling for registerPayment page

--- my-app/src/app/components/PaymentInfoForm.tsx
+++ my-app/src/app/components/PaymentInfoForm.tsx
@@ -2,16 +2,23 @@ import styles from './PaymentInfoForm.module.css'
 
 export default function PaymentInfoForm() {
     return (
-        <div className={styles.form}>
+        <div className={styles.formContainer}>
             <h1>Payment Information (Optional)</h1>
-            <form>
-                <label>Card Number: <input type="text" pattern="[0-9]*" name="cardnum"/></label>
-                <label>Expiration Date: <input type="date" name="date"/></label>
-                <label>CVV: <input type="number" name="cvv"/></label>
-                <label>Billing Address: <input type="text" name="address"/></label>
-                <label>City: <input type="text" name="city"/></label>
-                <label>State: <input type="text" name="state"/></label>
-                <label>Zip Code: <input type="number" name="zipcode"/></label>
+            <form className={styles.inputForm}>
+                <label>Card Number<input type="text" pattern="[0-9]*" name="cardNum" placeholder="Type here"/></label>
+                <div className={styles.otherCardInfo}>
+                    <label>Expiration Date<input type="date" name="date" placeholder="Type here"/></label>
+                    <label>CVV<input type="text" name="cvv" placeholder="Type here"/></label>
+                </div>
+                <label>Billing Addres <input type="text" name="address" placeholder="Type here"/></label>
+                <div className={styles.otherAddressInfo}>
+                    <label>City<input type="text" name="city" placeholder="Type here"/></label>
+                    <label>State<input type="text" name="state" placeholder="Type here"/></label>
+                    <label>Zip Code<input type="text" name="zipcode" placeholder="Type here"/></label>
+                </div>
+                <div className={styles.buttonContainer}>
+                    <input type="submit" value="Submit" className={styles.submitButton}/>
+                </div>
             </form>
         </div>
     );
--- my-app/src/app/registerPayment/RegisterPayment.tsx
+++ my-app/src/app/registerPayment/RegisterPayment.tsx
@@ -1,7 +1,14 @@
 import PaymentInfoForm from "../components/PaymentInfoForm";
+import styles from './RegisterPayment.module.css';
 
 export default function RegisterPayment() {
     return (
-        <PaymentInfoForm/>
+        <>
+            <div className={styles.headings}>
+                <h1>SlayTix</h1>
+                <h2>Sign Up Now!</h2>
+            </div>
+            <PaymentInfoForm/>
+        </>
     );
 }
