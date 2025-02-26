f8060c6a3f5a361ae544b047021622c48e870014
tmt83019 <thu7560@gmail.com>
2025-02-26 18:54:00 -0500

    Updated styling for registerAddress page

--- my-app/src/app/components/ShippingAddrForm.tsx
+++ my-app/src/app/components/ShippingAddrForm.tsx
@@ -2,14 +2,19 @@ import styles from './ShippingAddrForm.module.css';
 
 export default function ShippingAddrForm() {
     return (
-        <div className={styles.form}>
+        <div className={styles.formContainer}>
             <h1>Shipping Address (Optional)</h1>
-            <form>
-                <label>Street Address: <input type="text" name="streetaddr"/></label>
-                <label>Shipping Address: <input type="text" name="shipaddr"/></label>
-                <label>City: <input type="text" name="city"/></label>
-                <label>State: <input type="text" name="state"/></label>
-                <label>Zip Code: <input type="number" name="zipcode"/></label>
+            <form className={styles.inputForm}>
+                <label>Street Address: <input type="text" name="streetaddr" placeholder="Type here"/></label>
+                <label>Shipping Address: <input type="text" name="shipaddr" placeholder="Type here"/></label>
+                <div className={styles.cityState}>
+                    <label>City: <input type="text" name="city" placeholder="Type here"/></label>
+                    <label>State: <input type="text" name="state" placeholder="Type here"/></label>
+                </div>
+                <label>Zip Code: <input type="number" name="zipcode" placeholder="Type here"/></label>
+                <div className={styles.buttonContainer}>
+                    <input type="submit" value="Submit" className={styles.submitButton}/>
+                </div>
             </form>
         </div>
     );
--- my-app/src/app/registerAddress/RegisterAddress.tsx
+++ my-app/src/app/registerAddress/RegisterAddress.tsx
@@ -1,7 +1,14 @@
 import ShippingAddrForms from "../components/ShippingAddrForm";
+import styles from './RegisterAddress.module.css';
 
 export default function RegisterAddress() {
     return (
-        <ShippingAddrForms/>
+        <>
+            <div className={styles.headings}>
+                <h1>SlayTix</h1>
+                <h2>Sign Up Now!</h2>
+            </div>
+            <ShippingAddrForms/>
+        </>
     );
 }
