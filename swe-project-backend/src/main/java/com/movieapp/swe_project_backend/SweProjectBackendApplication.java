84e8108e5e8660b8bd3f051601c31d1c1c4b4028
ahuang4730 <ahuang4730@gmail.com>
2025-02-27 07:20:43 -0500

    Reuploading empty framework ig

---
+++ swe-project-backend/src/main/java/com/movieapp/swe_project_backend/SweProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.movieapp.swe_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweProjectBackendApplication.class, args);
+	}
+
+}
