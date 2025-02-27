055b46bea24db08fe9ef8b156b42218f3eb0e893
ahuang4730 <ahuang4730@gmail.com>
2025-02-27 09:16:59 -0500

    added cors.sh

---
+++ swe-project-backend/src/main/java/com/movieapp/swe_project_backend/controller/cors.sh
@@ -0,0 +1,6 @@
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000") // Allows frontend to call backend
+public class MyController { 
+    // API endpoints here 
+}
