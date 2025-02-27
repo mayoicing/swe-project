a85c78c540acf4cbd0f37c1a75929082a16d38c3
tmt83019 <thu7560@gmail.com>
2025-02-27 03:15:08 -0500

    Added genre buttons to homepage
    Buttons can be selected and unselected

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,11 +1,13 @@
 /* Page - Home */
 import Navbar from './components/Navbar'
 import MoviesList from './components/MoviesList';
+import GenreButtons from './components/GenreButtons';
 
 export default function Home() {
   return (
     <>
       <Navbar/>
+      <GenreButtons/>
       <MoviesList/>
     </>
   );
