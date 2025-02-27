77e262ed8091b91325e2031ff881d4bfb808b9e7
ahuang4730 <ahuang4730@gmail.com>
2025-02-27 07:18:34 -0500

    It's not working, I think I'm going to need to remake the files

--- backend-stuff/src/main/java/com/movieapp/swe_project_backend/repository/CastAndCrewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.movieapp.swe_project_backend.repository;
-
-import com.movieapp.swe_project_backend.model.CastAndCrew;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CastAndCrewRepository extends JpaRepository<CastAndCrew, Long> {
-}
