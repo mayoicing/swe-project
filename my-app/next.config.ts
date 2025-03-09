00870ea97a5280a9bdac85b8fab498c126b25d68
natalie20749 <nrm20749@uga.edu>
2025-03-09 16:27:57 -0400

    Search Function

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -2,7 +2,38 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["www.google.com", "imdb.com", "upload.wikimedia.org", "m.media-amazon.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: 'thefilmsmith.com',
+        pathname: '/wp-content/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        pathname: '/wikipedia/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imdb.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.google.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3.amazonaws.com',
+        pathname: '/nightjarprod/content/uploads/**',
+      },
+    ]
   }
   /* config options here */
 };
