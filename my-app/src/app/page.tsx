c0b12d92f75776ef02256dd881e079707093d376
Katie <katiedao3@gmail.com>
2025-03-21 16:04:23 -0400

    updated home page to look prettier

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,11 +2,13 @@
 import Navbar from './components/Navbar';
 import MoviesList from './components/MoviesList';
 import GenreButtons from './components/GenreButtons';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <>
       <Navbar />
+            <h2 className={styles.h2}>MOVIES</h2>
       <GenreButtons />
       <MoviesList />
     </>
