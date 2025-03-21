de69047bcdd517d6f097c582c874ae947ac880ca
tmt83019 <thu7560@gmail.com>
2025-03-21 05:14:47 -0400

    Added alert in forget password, added some navigation to edit forms,  added remember me option to both user & admin login

--- my-app/src/app/components/AdminLoginForm.tsx
+++ my-app/src/app/components/AdminLoginForm.tsx
@@ -8,14 +8,16 @@ export default function LoginForm() {
             <form action="/adminMovie" method="POST" className={styles.inputForm}>
                 <label>Email Address<input type="email" name="email" placeholder="Enter here" /></label>
                 <label>Password<input type="password" name="password" placeholder="Enter here" /></label>
+                <label>Remember me? <input type="checkbox" name="rememeberMe"/></label>
                 <div className={styles.buttonContainer}>
                     <input type="submit" value="Log In" className={styles.submitButton} />
                 </div>
             </form>
             <div className={styles.navigation}>
-                <Link href='/'>Forget password?</Link>
+                <Link href='/forgetPassword'>Forget password?</Link>
                 <Link href='/loginUser' className={styles.admin}>User Login</Link>
             </div>
+            <div className={styles.signup}><Link href='/registerPersonal'>Don't have an account? Create one here!</Link></div>
         </div>
     );
 }
--- my-app/src/app/components/LoginForm.tsx
+++ my-app/src/app/components/LoginForm.tsx
@@ -8,14 +8,18 @@ export default function LoginForm() {
             <form action="/" method="POST" className={styles.inputForm}>
                 <label>Email Address<input type="email" name="email" placeholder="Enter here"/></label>
                 <label>Password<input type="password" name="password" placeholder="Enter here"/></label>
+                <div className={styles.checkbox}>
+                    <label>Remember me? <input type="checkbox" name="rememeberMe"/></label>
+                </div>
                 <div className={styles.buttonContainer}>
                     <input type="submit" value="Log In" className={styles.submitButton}/>
                 </div>
             </form>
             <div className={styles.navigation}>
-                    <Link href='/'>Forget password?</Link>
+                    <Link href='/forgetPassword'>Forget password?</Link>
                     <Link href='/loginAdmin' className={styles.admin}>Admin Login</Link>
             </div>
+            <div className={styles.signup}><Link href='/registerPersonal'>Don't have an account? Create one here!</Link></div>
         </div>
     );
 }
