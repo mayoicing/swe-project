d619ad95da17e362bb365d3087290899b6a28502
tmt83019 <thu7560@gmail.com>
2025-03-22 19:29:36 -0400

    Added "Add Card" to places with no card

--- my-app/src/app/components/NoCard.tsx
+++ my-app/src/app/components/NoCard.tsx
@@ -1,7 +1,18 @@
+"use client";
 import styles from './NoCard.module.css';
+import { useState } from 'react';
 
 export default function NoCard() {
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleActive = () => {
+        setIsActive(!isActive);
+    };
+
     return(
-        <div className={styles.noCard}>No Card</div>
+        <div className={styles.noCard}>
+            No Card
+            <button className={styles.button}>Add New Card</button>
+        </div>
     );
 }
--- my-app/src/app/components/PaymentCard.tsx
+++ my-app/src/app/components/PaymentCard.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import styles from './PaymentCard.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
