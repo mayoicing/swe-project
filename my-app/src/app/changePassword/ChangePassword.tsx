9f8bfd5a16368026484fdcb71620e51fc5737996
natalie20749 <nrm20749@uga.edu>
2025-03-24 09:32:46 -0400

    redirected change password page to login page

--- my-app/src/app/changePassword/ChangePassword.tsx
+++ my-app/src/app/changePassword/ChangePassword.tsx
@@ -3,12 +3,14 @@
 import { useState } from 'react';
 import axios from 'axios';
 import styles from './ChangePassword.module.css';
+import { useRouter } from 'next/navigation';
 
 export default function ChangePassword() {
     const [email, setEmail] = useState("");
     const [code, setCode] = useState("");
     const [newPassword, setNewPassword] = useState("");
     const [message, setMessage] = useState("");
+    const router = useRouter();
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
@@ -18,7 +20,9 @@ export default function ChangePassword() {
                 resetCode: code,
                 newPassword
             });
+            alert("✅ Password updated!");
             setMessage("✅ Password updated!");
+            router.push("/loginUser");
         } catch (error) {
             setMessage("❌ Failed to reset password.");
             console.error(error);
