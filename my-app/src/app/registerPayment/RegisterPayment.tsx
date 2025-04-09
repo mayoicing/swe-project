abd88d3c442b1f05c6068805c6ff9a7b3516c487
tmt83019 <thu7560@gmail.com>
2025-04-09 00:03:20 -0400

    User can now click on 'SlayTix' to exit registration & back to homepage

--- my-app/src/app/registerPayment/RegisterPayment.tsx
+++ my-app/src/app/registerPayment/RegisterPayment.tsx
@@ -2,12 +2,13 @@
 
 import PaymentInfoForm from "../components/PaymentInfoForm";
 import styles from './RegisterPayment.module.css';
+import Link from "next/link";
 
 export default function RegisterPayment() {
     return (
         <>
             <div className={styles.headings}>
-                <h1>SlayTix</h1>
+                <h1><Link href='/'>SlayTix</Link></h1>
                 <h2>Sign Up Now!</h2>
             </div>
             <PaymentInfoForm/>
--- my-app/src/app/registerPersonal/RegisterPersonal.tsx
+++ my-app/src/app/registerPersonal/RegisterPersonal.tsx
@@ -2,12 +2,13 @@
 
 import PersonalInfoForm from "../components/PersonalInfoForm";
 import styles from './RegisterPersonal.module.css';
+import Link from "next/link";
 
 export default function RegisterPersonal() {
     return (
         <>
             <div className={styles.headings}>
-                <h1>SlayTix</h1>
+                <h1><Link href='/'>SlayTix</Link></h1>
                 <h2>Sign Up Now!</h2>
             </div>
             <PersonalInfoForm/>
